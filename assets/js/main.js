293250f54211ba850b8d35484ec92bf31a4a7503
Melizloor <lenny_loor@outlook.com>
2023-06-15 23:04:06 -0500

    disenio-responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 function Encriptar(){
     //Enviar informacion
     var text_encriotador = document.getElementById("send-info").value;
-    document.getElementById("get-info").value = text_encriotador;
+    document.getElementById("get-info").innerHTML = text_encriotador;
     //Ocultar secciones
     document.getElementById("info").style.display = "block";
     document.getElementById("info-empty").style.display = "none"; 
@@ -13,12 +13,8 @@ function Desencriptar(){
     document.getElementById("info-empty").style.display = "block";
     document.getElementById("info").style.display = "none"; 
 }
-function Copiar(){
-    //alert('Copiar');
-
-    var textEncriptado = document.getElementById("get-info").value; 
+function Copiar(){ 
+    var textEncriptado = document.getElementById("get-info").innerHTML;  
     document.getElementById("get-info").select();
-    navigator.clipboard.writeText(textEncriptado).then();
-    
-    
+    navigator.clipboard.writeText(textEncriptado).then();  
 }
